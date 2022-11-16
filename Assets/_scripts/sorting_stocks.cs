a546c95ea7828c7275526dfd4ef01c886ea8defc
Aman <solankibhavesh0149@gmail.com>
2022-11-16 20:17:41 +0530

    pc change

--- Assets/_scripts/sorting_stocks.cs
+++ Assets/_scripts/sorting_stocks.cs
@@ -36,14 +36,12 @@ public class sorting_stocks : MonoBehaviour
     {
         target = _target;
 
-
-        _current_index = _target.transform.GetSiblingIndex();
+        
+        _current_index = (_target.transform.parent == transform) ? (_target.transform.GetSiblingIndex()) : (_place_holder.transform.GetSiblingIndex());
         _index_to_check = _current_index - _AddOrRemove;
-
 
         while (_index_to_check > 0 && _index_to_check < transform.childCount && transform.GetChild(_index_to_check).GetComponent<stock_data>()._holdings * _AddOrRemove < _target.GetComponent<stock_data>()._holdings * _AddOrRemove)
             _index_to_check -= _AddOrRemove;
-
 
         if (_current_index - _AddOrRemove != _index_to_check)
         {
@@ -52,63 +50,12 @@ public class sorting_stocks : MonoBehaviour
             _place_holder.transform.SetSiblingIndex((_AddOrRemove == 1) ? (_index_to_check + 1) : (_index_to_check));
             _target.transform.SetParent(transform.parent);
 
-            StartCoroutine(moveTowards());
+            StartCoroutine(moveTowards(_AddOrRemove));
         }
 
     }
 
-    //public void OnAdd_holdings(GameObject changed_target)
-    //{
-
-    //    if (_place_holder.transform.parent != transform)
-    //    {
-    //        _place_holder.transform.SetParent(transform);
-    //        _place_holder.transform.SetSiblingIndex(changed_target.transform.GetSiblingIndex());
-    //    }
-
-    //    int above_values = _place_holder.transform.GetSiblingIndex() - 1;
-    //    while (above_values >= 0 && changed_target.GetComponent<stock_data>()._holdings > transform.GetChild(above_values).GetComponent<stock_data>()._holdings)
-    //    {
-    //        above_values -= 1;
-    //    }
-
-    //    if (above_values + 1 != _place_holder.transform.GetSiblingIndex())
-    //    {
-    //        print("add");
-    //        _place_holder.transform.SetSiblingIndex(above_values + 1);
-    //        _place_holder.SetActive(true);
-
-    //        changed_target.transform.SetParent(transform.parent.parent);
-    //        StartCoroutine(moveTowards(changed_target));
-    //    }
-    //}
-
-    //public void OnRemove_holdings(GameObject changed_target)
-    //{
-    //    if (_place_holder.transform.parent != transform)
-    //    {
-    //        _place_holder.transform.SetParent(transform);
-    //        _place_holder.transform.SetSiblingIndex(changed_target.transform.GetSiblingIndex() + 1);
-    //    }
-    //    int above_values = _place_holder.transform.GetSiblingIndex() + 1;
-    //    while (above_values < transform.childCount && changed_target.GetComponent<stock_data>()._holdings < transform.GetChild(above_values).GetComponent<stock_data>()._holdings)
-    //    {
-    //        above_values += 1;
-    //    }
-    //    if (above_values != _place_holder.transform.GetSiblingIndex())
-    //    {
-    //        print("in");
-    //        _place_holder.transform.SetSiblingIndex(above_values);
-    //        _place_holder.SetActive(true);
-
-    //        changed_target.transform.SetParent(transform.parent.parent);
-    //        StartCoroutine(moveTowards(changed_target));
-    //    }
-
-
-    //}
-
-    IEnumerator moveTowards()
+    IEnumerator moveTowards(int _AddOrRemove)
     {
         yield return new WaitForSeconds(0.1f);
 
@@ -117,14 +64,15 @@ public class sorting_stocks : MonoBehaviour
         while ((int)target.transform.position.y / 10 != (int)_place_holder.transform.position.y / 10)
         {
             new_scroll_post = transform.position;
-            target.transform.position = Vector3.Lerp(target.transform.position, _place_holder.transform.position, 0.1f) + new_scroll_post - old_scroll_post;
+            target.transform.position = Vector3.Lerp(target.transform.position, _place_holder.transform.position, 0.1f) - (new_scroll_post - old_scroll_post);
             old_scroll_post = transform.position;
 
             yield return new WaitForSeconds(0.02f);
         }
         _place_holder.SetActive(false);
         target.transform.SetParent(transform);
-        target.transform.SetSiblingIndex(_place_holder.transform.GetSiblingIndex());
+        target.transform.SetSiblingIndex((_AddOrRemove == 1) ? (_index_to_check + 1) : (_index_to_check));
+        print("new index-" + _place_holder.transform.GetSiblingIndex());
         _place_holder.transform.SetParent(this.transform.parent.parent);
 
         int upordown; //check if the is above below the mask
